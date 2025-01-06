400e9b94a9b290dc53ca00119106a2955f75e9f7
waldemarciupa <waldemarciupa90@gmail.com>
2025-01-06 13:41:04 +0100

    212-enum-roles solved

--- tasks/core-pro/212-enum-roles/task.ts
+++ tasks/core-pro/212-enum-roles/task.ts
@@ -1,13 +1,22 @@
-export enum UserPermission {}
-
-export enum UserRole {}
-
-export interface User {
-  role: string;
-  permissions: string[];
+export enum UserPermission {
+  VIEW = 'VIEW',
+  READ = 'READ',
+  WRITE = 'WRITE',
+  DELETE = 'DELETE',
 }
 
-export function hasAccess(user: User, requiredPermission: string): boolean {
+export enum UserRole {
+  ADMIN = 'ADMIN',
+  EDITOR = 'EDITOR',
+  VIEWER = 'VIEWER',
+}
+
+export interface User {
+  role: UserRole.ADMIN | UserRole.EDITOR | UserRole.VIEWER;
+  permissions: UserPermission[];
+}
+
+export function hasAccess(user: User, requiredPermission: UserPermission): boolean {
   if (user.role === 'ADMIN') return true;
   return user.permissions.includes(requiredPermission);
 }
