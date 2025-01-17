1d4536f9ef3ffb65fce8b03bbe26a9fb2ba5a693
waldemarciupa <waldemarciupa90@gmail.com>
2025-01-17 09:50:59 +0100

    311-generics-swapbox solved

--- tasks/core-pro/311-generics-swapbox/task.ts
+++ tasks/core-pro/311-generics-swapbox/task.ts
@@ -6,8 +6,8 @@ function swapStrings(a: string, b: string): [string, string] {
   return [b, a];
 }
 
-export function swapBox(a, b) {
-  throw new Error('Not implemented');
+export function swapBox<T>(a: T, b: T) {
+  return [b, a];
 }
 
 const [n1, n2] = swapNumbers(10, 20);
